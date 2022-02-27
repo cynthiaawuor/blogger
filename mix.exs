c9f7b73189574b6da233006502e4d97321f5edfd
cynthiaawuor <cynthiaawuor2000@gmail.com>
2022-02-28 00:11:41 +0300

    added hhtpoison to dependencies and applications

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Blogger.MixProject do
   def application do
     [
       mod: {Blogger.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :httpoison]
     ]
   end
 
@@ -44,11 +44,13 @@ defmodule Blogger.MixProject do
       {:phoenix_live_dashboard, "~> 0.5"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
+      {:httpoison, "~> 1.8"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
+
     ]
   end
 
